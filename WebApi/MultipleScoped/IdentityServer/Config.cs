1096a207d23ed5b5d3419ac573e86c7faf36b0f0
sheeri185adidam <sheeri185adidam@gmail.com>
2019-12-15 12:53:39 -0800

    more changes w.r.t. IdentityServer UI and projects renaming and ordering.

---
+++ WebApi/MultipleScoped/IdentityServer/Config.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4.Models;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace IdentityServer
+{
+	/// <summary>
+	/// A configuration.
+	/// </summary>
+	public static class Config
+	{
+		/// <summary>
+		/// Gets the identifiers.
+		/// </summary>
+		/// <value>The identifiers.</value>
+		public static IEnumerable<IdentityResource> Ids => new IdentityResource[]
+		{
+			new IdentityResources.OpenId()
+		};
+
+		/// <summary>
+		/// Gets the apis.
+		/// </summary>
+		/// <value>The apis.</value>
+		public static IEnumerable<ApiResource> Apis => new ApiResource[]
+		{
+			new ApiResource("identity_api", "Protected Simple API")
+		};
+
+		/// <summary>
+		/// Gets the clients.
+		/// </summary>
+		/// <value>The clients.</value>
+		public static IEnumerable<Client> Clients => new Client[]
+		{
+			new Client
+			{
+				ClientId = "simple_client",
+				AllowedGrantTypes = GrantTypes.ClientCredentials,
+				ClientSecrets =
+				{
+					new Secret("ngp".Sha256())
+				},
+				AllowedScopes = { "identity_api"}
+			}
+		};
+	}
+}
---
+++ WebApi/SingleScoped/WebApi/Controllers/IdentityController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiApp.Controllers
+{
+	/// <summary>
+	/// A controller for handling identities.
+	/// </summary>
+	/// <seealso cref="T:Microsoft.AspNetCore.Mvc.ControllerBase"/>
+    [Route("identity")]
+	[Authorize]
+	public class IdentityController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+		}
+	}
+}
