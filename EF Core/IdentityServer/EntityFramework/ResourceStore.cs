e9d7e96c12602b159faa92619bcd17be27b5921c
Manik Sheeri <sheeri185adidam@gmail.com>
2019-12-19 09:34:10 -0800

    fixed compilation errors

--- EF Core/IdentityServer/EntityFramework/ResourceStore.cs
+++ EF Core/IdentityServer/EntityFramework/ResourceStore.cs
@@ -1,9 +1,8 @@
 using IdentityServer4.Models;
-using Newtonsoft.Json;
+using IdentityServer4.Stores;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
